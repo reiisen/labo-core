b6fda026cfcaa44b6a727762a5f52f05f8671333
reiisen <kentjot@gmail.com>
2024-12-18 11:07:08 +0700

    more v

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import setupRoute from './src/route/router'
+import cors from 'cors';
+import config from './config.json';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: `${process.env.FRONTEND_URL ? process.env.FRONTEND_URL : process.env.APP_URL}` }));
+
+const port = process.env.PORT;
+
+setupRoute(app);
+
+app.listen(port, () => {
+  console.clear();
+  console.log(`
+    🚀 Server ready at: http://localhost:${config.serverPort}
+  `);
+})
