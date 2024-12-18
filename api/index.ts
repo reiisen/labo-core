8cc4bbc7b4644a55972aa82af8e9a305c9c1117e
reiisen <kentjot@gmail.com>
2024-12-18 13:04:38 +0700

    cors

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,8 @@ import config from '../config.json';
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: `${process.env.FRONTEND_URL ? process.env.FRONTEND_URL : process.env.APP_URL}` }));
+// app.use(cors({ origin: `${process.env.FRONTEND_URL ? process.env.FRONTEND_URL : process.env.APP_URL}` }));
+app.use(cors({ origin: '*' }));
 
 const port = process.env.PORT;
 
